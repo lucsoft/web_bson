18670e73cbc788a1eb9380c9f98d1aa9dd2c0b29
ERFANIUM <erfanshield@outlook.com>
2021-12-16 23:25:17 +0330

    chore(benchmarks): use static imports

--- benchmarks/simpleSerializeDeserialize.ts
+++ benchmarks/simpleSerializeDeserialize.ts
@@ -1,16 +1,14 @@
-const deno_bson = await import(
-  "https://raw.githubusercontent.com/lucsoft/deno_bson/master/mod.ts"
-);
-const jsBson = await import(
-  "https://cdn.skypack.dev/pin/bson@v4.5.4-F38JF4sWndM08oQ5WCDI/mode=imports/optimized/bson.js"
-);
-const localBson = await import("../mod.ts");
+// deno-lint-ignore-file no-explicit-any
+import * as denoBson from "https://raw.githubusercontent.com/lucsoft/deno_bson/master/mod.ts";
+import * as jsBson from "https://cdn.skypack.dev/bson";
+import * as localBson from "../mod.ts";
 import {
   bench,
   BenchmarkTimer,
   prettyBenchmarkProgress,
   runBenchmarks,
 } from "../test_deps.ts";
+
 const runs = 2000;
 
 function BenchCode(timer: BenchmarkTimer, Bson: any) {
@@ -22,13 +20,12 @@ function BenchCode(timer: BenchmarkTimer, Bson: any) {
     foo: [] as any[],
   };
 
-  /// 1k times
   for (let j = 0; j < 300; j++) {
     d.foo.push({
       bar: new Bson.ObjectId(),
       name: "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
       title: "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
-      a: [1, 45, , 78, 45, 78, 23, 13, 545, 3, 45],
+      a: [1, 45, 78, 45, 78, 23, 13, 545, 3, 45],
     });
   }
 
@@ -37,7 +34,7 @@ function BenchCode(timer: BenchmarkTimer, Bson: any) {
   timer.stop();
 }
 
-bench({ runs, name: "master", func: (b) => BenchCode(b, deno_bson) });
+bench({ runs, name: "master", func: (b) => BenchCode(b, denoBson) });
 bench({ runs, name: "js-bson", func: (b) => BenchCode(b, jsBson) });
 bench({ runs, name: "local", func: (b) => BenchCode(b, localBson) });
 runBenchmarks({ silent: true, skip: /_long/ }, prettyBenchmarkProgress());
