4363e4d2692f94d6819bf0bc21be7af09b904291
GitHub <noreply@github.com>
2022-01-13 08:47:19 +0000

    Update dependencies

--- test_deps.ts
+++ test_deps.ts
@@ -3,13 +3,13 @@ export {
   assertEquals,
   assertThrows,
   equal,
-} from "https://deno.land/std@0.120.0/testing/asserts.ts";
+} from "https://deno.land/std@0.121.0/testing/asserts.ts";
 export {
   bench,
   runBenchmarks,
-} from "https://deno.land/std@0.120.0/testing/bench.ts";
-export type { BenchmarkTimer } from "https://deno.land/std@0.120.0/testing/bench.ts";
+} from "https://deno.land/std@0.121.0/testing/bench.ts";
+export type { BenchmarkTimer } from "https://deno.land/std@0.121.0/testing/bench.ts";
 export {
   prettyBenchmarkProgress,
 } from "https://deno.land/x/pretty_benching@v0.3.3/mod.ts";
-export * as UUID from "https://deno.land/std@0.120.0/uuid/mod.ts";
+export * as UUID from "https://deno.land/std@0.121.0/uuid/mod.ts";
