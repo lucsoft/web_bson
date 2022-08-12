27e220d0397c90738024c5ff4058f36b86509de2
GitHub <noreply@github.com>
2022-08-12 08:46:52 +0000

    Update dependencies

--- test_deps.ts
+++ test_deps.ts
@@ -3,6 +3,6 @@ export {
   assertEquals,
   assertThrows,
   equal,
-} from "https://deno.land/std@0.151.0/testing/asserts.ts";
-export * as UUID from "https://deno.land/std@0.151.0/uuid/mod.ts";
+} from "https://deno.land/std@0.152.0/testing/asserts.ts";
+export * as UUID from "https://deno.land/std@0.152.0/uuid/mod.ts";
 export * as jsBson from "https://cdn.skypack.dev/bson";
