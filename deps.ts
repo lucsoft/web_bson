a0184d557d8071b05385f6545c2472b431aa2278
GitHub <noreply@github.com>
2022-11-17 08:47:28 +0000

    Update dependencies

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export * as hex from "https://deno.land/std@0.164.0/encoding/hex.ts";
-export * as b64 from "https://deno.land/std@0.164.0/encoding/base64.ts";
-export * from "https://deno.land/std@0.164.0/bytes/mod.ts";
+export * as hex from "https://deno.land/std@0.165.0/encoding/hex.ts";
+export * as b64 from "https://deno.land/std@0.165.0/encoding/base64.ts";
+export * from "https://deno.land/std@0.165.0/bytes/mod.ts";
