d227f9d6faad290e670e5ad798ea962971e0e0a4
lucsoft <mail@lucsoft.de>
2023-02-01 19:27:55 +0100

    chore: Remove all refs of old code

--- deps.ts
+++
@@ -1,3 +0,0 @@
-export * as hex from "https://deno.land/std@0.174.0/encoding/hex.ts";
-export * as b64 from "https://deno.land/std@0.174.0/encoding/base64.ts";
-export * from "https://deno.land/std@0.174.0/bytes/mod.ts";
--- test/string_test.ts
+++
@@ -1,8 +0,0 @@
-import { deserialize, serialize } from "../src/bson.ts";
-
-Deno.test("[string tests] can serialize and deserialize 0xFFFD", () => {
-  const unicodeString = String.fromCharCode(0x41, 0x42, 0xfffd, 0x43, 0x44); // "AB�CD"
-
-  const serialized = serialize({ value: unicodeString });
-  deserialize(serialized);
-});
